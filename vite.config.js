9b46b23dee94208ababe3c42dc5e69d101020592
umaedi <umaedi.kh.99@gmail.com>
2025-10-08 22:26:06 +0700

    path build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import path from "path";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  plugins: [
+    laravel({
+      input: ["resources/css/app.css", "resources/js/app.js"],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: path.resolve(__dirname, "../public_html/kasir-app/build"),
+    emptyOutDir: true,
+  },
 });

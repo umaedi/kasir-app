955091ee6f663fdb772f37c58bf5f0a901cdf525
umaedi <umaedi.kh.99@gmail.com>
2025-10-10 21:34:15 +0700

    title

--- resources/views/cms/layouts/app.blade.php
+++ resources/views/cms/layouts/app.blade.php
@@ -7,7 +7,8 @@
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>{{ $title }}</title>
+    <title>{{ $title ?? 'Dashboard Admin' }}</title>
+
 
     <meta name="description" content="" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
